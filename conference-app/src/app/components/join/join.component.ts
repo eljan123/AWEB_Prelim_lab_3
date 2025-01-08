58dd5460705c11b61f99cae60770244a7d703342
Eljan <108778203+eljan123@users.noreply.github.com>
2025-01-08 15:06:33 +0800

    Add files via upload

---
+++ conference-app/src/app/components/join/join.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-join',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './join.component.html',
+  styleUrl: './join.component.css'
+})
+export class JoinComponent {
+
+}
